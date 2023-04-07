f8ee6588baf2e3183342ffa9e77a9acc915ff8dd
lenamagdasalim1983 <130156228+lenamagdasalim1983@users.noreply.github.com>
2023-04-07 19:57:00 +0700

    Initial Drumkit Website
    The website can play audio based on keyboard stroke and button click

---
+++ js/index.js
@@ -0,0 +1,38 @@
+for (var i=0; i< document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+function handleClick(){
+    var strButtonClicked= this.innerHTML;
+    makeSound(strButtonClicked);
+}
+
+document.addEventListener("keydown", function(e){
+    handleKey(e);
+});
+function handleKey(eventMine){
+    var myKey=eventMine.key;
+    makeSound(myKey);
+}
+
+function makeSound(strChar){
+    switch (strChar){
+        case "w":var audioMine=new Audio("sounds/crash.mp3");
+            break;
+        case "a":var audioMine=new Audio("sounds/kick-bass.mp3");
+            break;
+        case "s":var audioMine=new Audio("sounds/snare.mp3");
+            break;
+        case "d":var audioMine=new Audio("sounds/tom-1.mp3");
+            break;
+        case "j":var audioMine=new Audio("sounds/tom-2.mp3");
+            break;
+        case "k":var audioMine=new Audio("sounds/tom-3.mp3");
+            break;
+        case "l":var audioMine=new Audio("sounds/tom-4.mp3");
+            break;
+    }    
+    audioMine.play();
+}
+
+
+
